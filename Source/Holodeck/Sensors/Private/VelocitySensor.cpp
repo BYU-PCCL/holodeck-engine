2bd50134136f8f49adb549b98e02c1affc3e18b2
Mitchell Mortensen <mitchneutron@gmail.com>
2017-12-30 09:16:36 -0800

    Parent is now an AActor*, redid how the velocity is retrieved, and removed several member variables

--- Source/Holodeck/Sensors/Private/VelocitySensor.cpp
+++ Source/Holodeck/Sensors/Private/VelocitySensor.cpp
@@ -8,27 +8,12 @@ UVelocitySensor::UVelocitySensor() {
 void UVelocitySensor::BeginPlay() {
 	Super::BeginPlay();
 	//You need to get the pointer to the object the sensor is attached to. 
-	Parent = this->GetAttachParent();
-	if (Parent != nullptr) {
-		CurrentLocation = Parent->GetComponentLocation();
-	}
-	
-}
-
-//TODO(mitch) : Figure out if Unreal has a function to get the absolute velocity of a USceneComponent, instead of manually calculating it each tick. 
-FVector UVelocitySensor::GetVelocity() {
-	FVector DeltaLocation = CurrentLocation - LocationAtPreviousTick;
-	FVector Velocity = DeltaLocation / ChangeInTime;
-	return Velocity;
+	Parent = this->GetAttachmentRootActor();
 }
 
 void UVelocitySensor::TickSensorComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
 	if (Parent != nullptr && bOn) {
-		//VelocitySensor needs to know its previous location, current location, and how much time has passed in order to calculate its velocity.
-		this->ChangeInTime = DeltaTime;
-		this->LocationAtPreviousTick = this->CurrentLocation;
-		this->CurrentLocation = Parent->GetComponentLocation();
-		FVector Velocity = GetVelocity();
+		FVector Velocity = Parent->GetVelocity();
 		float* FloatBuffer = static_cast<float*>(Buffer);
 		FloatBuffer[0] = Velocity.X;
 		FloatBuffer[1] = Velocity.Y;
