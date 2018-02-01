3493e9f2c8d0b269b2a5e3d942fac14451997f4d
Mitchell Mortensen <mitchneutron@gmail.com>
2018-01-31 23:12:10 -0800

    Added hitdelegate and onhit() to bind to it

--- Source/Holodeck/Sensors/Private/CollisionSensor.cpp
+++ Source/Holodeck/Sensors/Private/CollisionSensor.cpp
@@ -8,13 +8,24 @@ UCollisionSensor::UCollisionSensor() {
 void UCollisionSensor::BeginPlay() {
 	Super::BeginPlay();
 	//You need to get the pointer to the object the sensor is attached to. 
-	Parent = this->GetAttachmentRootActor();
+	Parent = this->GetOwner();
+
+	//Set the hit delegate. Give it to the parent. 
+	FScriptDelegate HitDelegate;
+	HitDelegate.BindUFunction(this, TEXT("OnHit"));
+	this->GetAttachmentRootActor()->OnActorHit.AddUnique(HitDelegate);
+	
 }
 
 void UCollisionSensor::TickSensorComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
 	if (Parent != nullptr && bOn) {
-		bool IsColliding = Parent->GetVelocity();
 		bool* BoolBuffer = static_cast<bool*>(Buffer);
-		
+		BoolBuffer[0] = bIsColliding;
 	}
+	//bIsColliding = false;
+}
+
+void UCollisionSensor::OnHit(AActor* SelfActor, AActor* OtherActor, FVector NormalImpulse, const FHitResult& Hit) {
+	bIsColliding = true;
+	UE_LOG(LogHolodeck, Log, TEXT("UCollisionSensor::OnHit was called. Hit detected!"));
 }
