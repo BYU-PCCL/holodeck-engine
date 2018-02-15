48e77f902379f857e0549fb56ad4339a59aaa181
Mitchell Mortensen <mitchneutron@gmail.com>
2018-02-15 16:49:48 -0700

    Created stubs for Pixel Camera

---
+++ Source/Holodeck/Sensors/Private/PixelCamera.cpp
@@ -0,0 +1,15 @@
+#include "Holodeck.h"
+#include "PixelCamera.h"
+
+UPixelCamera::UPixelCamera() {
+
+}
+
+void UPixelCamera::Capture() {
+
+}
+
+void UPixelCamera::BeginPlay() {
+	Super::BeginPlay();
+
+}
---
+++ Source/Holodeck/Sensors/Public/PixelCamera.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "Holodeck.h"
+
+#include "HolodeckCamera.h"
+#include "PixelCamera.generated.h"
+
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class HOLODECK_API UPixelCamera : public UHolodeckCamera {
+	GENERATED_BODY()
+
+public:
+		UPixelCamera();
+
+		virtual void Capture() override;
+
+		virtual void BeginPlay() override;
+
+protected:
+	//Check out HolodeckSensor.h for definitions for this overridden functions. 
+	virtual FString GetDataKey() { return "PixelCamera"; };
+	virtual int GetNumItems() { return CaptureWidth * CaptureHeight; };
+	virtual int GetItemSize() { return sizeof(float); };
+
+
+private:
+
+
+};
