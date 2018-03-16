0ee498a8b252198ee079ad2763a70c5f8c8fa1ce
nickwalton <nickwalton00@gmail.com>
2018-03-15 22:57:40 -0600

    Got Android Functionality Working

--- Source/Holodeck/Agents/Private/Android.cpp
+++ Source/Holodeck/Agents/Private/Android.cpp
@@ -3,6 +3,11 @@
 #include "Holodeck.h"
 #include "Android.h"
 
+// Used to convert unreal torque unit (kg*cm^2/s^2) to (kg*m^2/s^2)
+const float CM_TORQUE_TO_M_TORQUE = 10000;
+
+const int NUM_JOINTS = 48;
+
 AAndroid::AAndroid() {
 	PrimaryActorTick.bCanEverTick = true;
 	bCollisionsAreVisible = false;
@@ -10,11 +15,16 @@ AAndroid::AAndroid() {
 
 void AAndroid::BeginPlay() {
 	Super::BeginPlay();
+	count = 0;
+	SkeletalMesh = Cast<USkeletalMeshComponent>(RootComponent);
+	BoneNames = SkeletalMesh->GetAllSocketNames();
+	Constraints = SkeletalMesh->Constraints;
 }
 
 // Called every frame
 void AAndroid::Tick(float DeltaTime) {
 	Super::Tick( DeltaTime );
+	ApplyTorques();
 }
 
 void AAndroid::NotifyHit(UPrimitiveComponent* MyComp,
@@ -44,4 +54,37 @@ void AAndroid::SetCollisionsVisible(bool Visible){
 
 bool AAndroid::GetCollisionsVisible() {
 	return bCollisionsAreVisible;
+}
+
+void AAndroid::ApplyTorques() {
+
+	
+
+	for (int i = 0; i < Constraints.Num(); i++) {
+
+		int BegComInd = i * 3;
+		FName JointName = Constraints[i]->JointName;
+
+		// TODO Find swing1,swing2,twist torque vectors
+		FRotator JointRotation = SkeletalMesh->GetSocketRotation(JointName);
+
+		FConstraintInstance* Constraint = Constraints[i];
+
+		FTransform JointTransform = Constraint->GetRefFrame(EConstraintFrame::Frame1);
+
+		FVector Swing1TorqVec = JointTransform.GetScaledAxis(EAxis::X);
+		FVector Swing2TorqVec = JointTransform.GetScaledAxis(EAxis::Y);
+		FVector TwistTorqVec = JointTransform.GetScaledAxis(EAxis::Z);
+
+		// Convert Torque magnitude to (Kg*m^2)/(s^2) and scale with Commands
+		Swing1TorqVec *= (CM_TORQUE_TO_M_TORQUE * CommandArray[BegComInd]);
+		Swing2TorqVec *= (CM_TORQUE_TO_M_TORQUE * CommandArray[BegComInd + 1]);
+		TwistTorqVec *= (CM_TORQUE_TO_M_TORQUE * CommandArray[BegComInd + 2]);
+
+		SkeletalMesh->AddTorque(Swing1TorqVec, JointName, false);
+		SkeletalMesh->AddTorque(Swing2TorqVec, JointName, false);
+		SkeletalMesh->AddTorque(TwistTorqVec, JointName, false);
+
+		UE_LOG(LogTemp, Warning, TEXT("Added Torque for Bone %s"), JointName.SafeString);
+	}
 }
