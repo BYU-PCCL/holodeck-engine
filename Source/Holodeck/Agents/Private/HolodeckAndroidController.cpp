84432f097ebc661f0e989e7dd94efa63cc43a61a
Nick Walton <nickwalton00@gmail.com>
2018-04-11 20:30:13 -0600

    Update HolodeckAndroidController.cpp

--- Source/Holodeck/Agents/Private/HolodeckAndroidController.cpp
+++ Source/Holodeck/Agents/Private/HolodeckAndroidController.cpp
@@ -25,36 +25,4 @@ void AHolodeckAndroidController::ExecuteCommand() {
 	if (AndroidPawn != nullptr && FloatPtr != nullptr) {
 		AndroidPawn->CommandArray = FloatPtr;
 	}
-
-<<<<<<< HEAD
-=======
-		// Get rotation of that socket
-		FQuat RotQuat = SkeletalMesh->GetSocketQuaternion(JointName);
-
-		// Apply Swing 1 Torque if non zero
-		if (CommandArray[ComInd] != 0) {
-			float RotForce = CommandArray[ComInd] * CM_TORQUE_TO_M_TORQUE;
-			SkeletalMesh->AddTorque(RotQuat.RotateVector(FVector(0.0f, 0.0f, RotForce)), JointName, false);
-		}
-		ComInd++;
-
-		// Apply Swing 2 if Torque non zero and is 2 or 3 axis joint
-		if (JointInd < (AndroidPawn->NUM_2_PLUS_3_AXIS_JOINTS)) {
-			if (CommandArray[ComInd] != 0) {
-				float RotForce = CommandArray[ComInd] * CM_TORQUE_TO_M_TORQUE;
-				SkeletalMesh->AddTorque(RotQuat.RotateVector(FVector(0.0f, RotForce, 0.0f)), JointName, false);
-			}
-			ComInd++;
-
-			// Apply Twist if Torque non zero and is 3 axis joint
-			if (JointInd < AndroidPawn->NUM_3_AXIS_JOINTS) {
-				if (CommandArray[ComInd] != 0) {
-					float RotForce = CommandArray[ComInd] * CM_TORQUE_TO_M_TORQUE;
-					SkeletalMesh->AddTorque(RotQuat.RotateVector(FVector(RotForce, 0.0f, 0.0f)), JointName, false);
-				}
-				ComInd++;
-			}
-		}
-	}
->>>>>>> 255bf186d85d93ad1ff3df8cc29b8e552981befb
 }
