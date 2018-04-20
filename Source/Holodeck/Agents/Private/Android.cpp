489c0f6a8db25092e073d8a677da045774a5c57a
nickwalton <nickwalton00@gmail.com>
2018-04-20 11:17:42 -0600

    Updated android  files

--- Source/Holodeck/Agents/Private/Android.cpp
+++ Source/Holodeck/Agents/Private/Android.cpp
@@ -3,6 +3,9 @@
 #include "Holodeck.h"
 #include "Android.h"
 
+// Used to convert unreal torque unit (kg*cm^2/s^2) to (kg*m^2/s^2)
+const float CM_TORQUE_TO_M_TORQUE = 10000;
+
 AAndroid::AAndroid() {
 	PrimaryActorTick.bCanEverTick = true;
 	bCollisionsAreVisible = false;
@@ -10,38 +13,138 @@ AAndroid::AAndroid() {
 
 void AAndroid::BeginPlay() {
 	Super::BeginPlay();
+	SkeletalMesh = Cast<USkeletalMeshComponent>(RootComponent);
 }
 
-// Called every frame
 void AAndroid::Tick(float DeltaTime) {
-	Super::Tick( DeltaTime );
+	Super::Tick(DeltaTime);
+	ApplyTorques();
 }
 
 void AAndroid::NotifyHit(UPrimitiveComponent* MyComp,
-						 AActor* Other,
-						 UPrimitiveComponent* OtherComp,
-						 bool bSelfMoved,
-						 FVector HitLocation,
-						 FVector HitNormal,
-						 FVector NormalImpulse,
-						 const FHitResult& Hit) {
+	AActor* Other,
+	UPrimitiveComponent* OtherComp,
+	bool bSelfMoved,
+	FVector HitLocation,
+	FVector HitNormal,
+	FVector NormalImpulse,
+	const FHitResult& Hit) {
 	if (bCollisionsAreVisible) {
 		UMaterialInstanceDynamic* TheMaterial_Dyn = UMaterialInstanceDynamic::Create(CollisionDecalMaterial, this);
-		
+
 		//FRotator rotator = (HitLocation.Rotation());
 		if (TheMaterial_Dyn != NULL) //FVector needs to be at least around 7,7,7 for some reason
 			UGameplayStatics::SpawnDecalAttached(TheMaterial_Dyn, FVector(7.5, 7.5, 7.5),
-												 MyComp, Hit.BoneName, HitLocation, HitLocation.Rotation(),
-												 EAttachLocation::KeepWorldPosition, 1.0);
+				MyComp, Hit.BoneName, HitLocation, HitLocation.Rotation(),
+				EAttachLocation::KeepWorldPosition, 1.0);
 		else
 			GEngine->AddOnScreenDebugMessage(-1, 3.f, FColor::Green, "Material for decal is null");
 	}
 }
 
-void AAndroid::SetCollisionsVisible(bool Visible){
+void AAndroid::SetCollisionsVisible(bool Visible) {
 	bCollisionsAreVisible = Visible;
 }
 
 bool AAndroid::GetCollisionsVisible() {
 	return bCollisionsAreVisible;
 }
+
+void AAndroid::ApplyTorques() {
+
+	int ComInd = 0;
+
+	for (int JointInd = 0; JointInd < NUM_JOINTS; JointInd++) {
+
+		FName JointName = Joints[JointInd];
+
+		// Get rotation of that socket
+		FQuat RotQuat = SkeletalMesh->GetSocketQuaternion(JointName);
+
+		// Apply Swing 1 Torque if non zero
+		if (CommandArray[ComInd] != 0) {
+			float RotForce = CommandArray[ComInd] * CM_TORQUE_TO_M_TORQUE;
+			SkeletalMesh->AddTorque(RotQuat.RotateVector(FVector(0.0f, 0.0f, RotForce)), JointName, false);
+		}
+		ComInd++;
+
+		// Apply Swing 2 if Torque non zero and is 2 or 3 axis joint
+		if (JointInd < (NUM_2_PLUS_3_AXIS_JOINTS)) {
+			if (CommandArray[ComInd] != 0) {
+				float RotForce = CommandArray[ComInd] * CM_TORQUE_TO_M_TORQUE;
+				SkeletalMesh->AddTorque(RotQuat.RotateVector(FVector(0.0f, RotForce, 0.0f)), JointName, false);
+			}
+			ComInd++;
+
+			// Apply Twist if Torque non zero and is 3 axis joint
+			if (JointInd < NUM_3_AXIS_JOINTS) {
+				if (CommandArray[ComInd] != 0) {
+					float RotForce = CommandArray[ComInd] * CM_TORQUE_TO_M_TORQUE;
+					SkeletalMesh->AddTorque(RotQuat.RotateVector(FVector(RotForce, 0.0f, 0.0f)), JointName, false);
+				}
+				ComInd++;
+			}
+		}
+	}
+}
+
+const FName AAndroid::Joints[] = {
+
+	// Head, Spine, and Arm joints. Each has [swing1, swing2, twist]
+	FName(TEXT("head")),
+	FName(TEXT("neck_01")),
+	FName(TEXT("spine_02")),
+	FName(TEXT("spine_01")),
+	FName(TEXT("upperarm_l")),
+	FName(TEXT("lowerarm_l")),
+	FName(TEXT("hand_l")),
+	FName(TEXT("upperarm_r")),
+	FName(TEXT("lowerarm_r")),
+	FName(TEXT("hand_r")),
+
+	// Leg Joints. Each has [swing1, swing2, twist]
+	FName(TEXT("thigh_l")),
+	FName(TEXT("calf_l")),
+	FName(TEXT("foot_l")),
+	FName(TEXT("ball_l")),
+	FName(TEXT("thigh_r")),
+	FName(TEXT("calf_r")),
+	FName(TEXT("foot_r")),
+	FName(TEXT("ball_r")),
+
+	// First joint of each finger. Has only [swing1, swing2]
+	FName(TEXT("thumb_01_l")),
+	FName(TEXT("index_01_l")),
+	FName(TEXT("middle_01_l")),
+	FName(TEXT("ring_01_l")),
+	FName(TEXT("pinky_01_l")),
+	FName(TEXT("thumb_01_r")),
+	FName(TEXT("index_01_r")),
+	FName(TEXT("middle_01_r")),
+	FName(TEXT("ring_01_r")),
+	FName(TEXT("pinky_01_r")),
+
+	// Second joint of each finger. Has only [swing1]
+	FName(TEXT("thumb_02_l")),
+	FName(TEXT("index_02_l")),
+	FName(TEXT("middle_02_l")),
+	FName(TEXT("ring_02_l")),
+	FName(TEXT("pinky_02_l")),
+	FName(TEXT("thumb_02_r")),
+	FName(TEXT("index_02_r")),
+	FName(TEXT("middle_02_r")),
+	FName(TEXT("ring_02_r")),
+	FName(TEXT("pinky_02_r")),
+
+	// Third joint of each finger. Has only [swing1]
+	FName(TEXT("thumb_03_l")),
+	FName(TEXT("index_03_l")),
+	FName(TEXT("middle_03_l")),
+	FName(TEXT("ring_03_l")),
+	FName(TEXT("pinky_03_l")),
+	FName(TEXT("thumb_03_r")),
+	FName(TEXT("index_03_r")),
+	FName(TEXT("middle_03_r")),
+	FName(TEXT("ring_03_r")),
+	FName(TEXT("pinky_03_r")),
+};
--- Source/Holodeck/Agents/Public/Android.h
+++ Source/Holodeck/Agents/Public/Android.h
@@ -17,53 +17,80 @@ class HOLODECK_API AAndroid : public AHolodeckAgent
 
 public:
 	/**
-	  * Default Constructor
-	  */
+	* Default Constructor
+	*/
 	AAndroid();
 
+	static const int NUM_JOINTS = 48;
+	static const int NUM_3_AXIS_JOINTS = 18;
+	static const int NUM_2_AXIS_JOINTS = 10;
+	static const int NUM_1_AXIS_JOINTS = 20;
+	static const int NUM_2_PLUS_3_AXIS_JOINTS = 28;
+
+	const static FName Joints[];
+
 	/**
-	  * Called when the game starts.
-	  */
+	* Called when the game starts.
+	*/
 	virtual void BeginPlay() override;
-	
+
 	/**
-	  * Tick
-	  * Called every frame.
-	  * @param DeltaSeconds the time since the last tick.
-	  */
+	* Tick
+	* Called every frame.
+	* @param DeltaSeconds the time since the last tick.
+	*/
 	void Tick(float DeltaSeconds) override;
 
 	/**
-	  * TODO(joshgreaves) : Explain this function properly.
-	  * NotifyHit
-	  * Used for the pressure sensor.
-	  */
+	* TODO(joshgreaves) : Explain this function properly.
+	* NotifyHit
+	* Used for the pressure sensor.
+	*/
 	void NotifyHit(UPrimitiveComponent* MyComp,
-				   AActor* Other,
-				   UPrimitiveComponent* OtherComp,
-				   bool bSelfMoved,
-				   FVector HitLocation,
-				   FVector HitNormal,
-				   FVector NormalImpulse,
-				   const FHitResult& Hit) override;
+		AActor* Other,
+		UPrimitiveComponent* OtherComp,
+		bool bSelfMoved,
+		FVector HitLocation,
+		FVector HitNormal,
+		FVector NormalImpulse,
+		const FHitResult& Hit) override;
 
 	//Decal material. This is used to show collisions on the Android. It is to be left blank and is set programmatically
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Materials)
-	UMaterial* CollisionDecalMaterial;
+		UMaterial* CollisionDecalMaterial;
 
 	/**
-	  * SetCollisionsVisible
-	  * Sets whether collisions with the Android can be seen.
-	  * @param Visible true to be seen.
-	  */
+	* SetCollisionsVisible
+	* Sets whether collisions with the Android can be seen.
+	* @param Visible true to be seen.
+	*/
 	void SetCollisionsVisible(bool Visible);
 
 	/**
-	  * GetCollisionsVisible
-	  * @return true if collisions are visible.
-	  */
+	* GetJoints
+	* Gets pointer to constant array of FName joints
+	* @return array of FName corresponding to android joint names
+	*/
+	const FName* GetJoints();
+
+	/**
+	* GetCollisionsVisible
+	* @return true if collisions are visible.
+	*/
 	bool GetCollisionsVisible();
+
+	float* CommandArray;
+
+	UPROPERTY(BlueprintReadWrite, Category = AndroidMesh)
+		USkeletalMeshComponent* SkeletalMesh;
 
 private:
 	bool bCollisionsAreVisible;
+
+	/**
+	* ApplyTorques
+	* Applies torques for that tick on each joint with a force/direction
+	* corresponding to the values in the command array
+	*/
+	void ApplyTorques();
 };
