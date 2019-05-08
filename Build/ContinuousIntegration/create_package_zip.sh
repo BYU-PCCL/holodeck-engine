f3e0a9b8ccedaaeb4c52ab1a944884c8dd5274eb
Jayden Milne <jaydenmilne@users.noreply.github.com>
2019-05-08 16:08:57 -0600

    Move configs to correct location

--- Build/ContinuousIntegration/create_package_zip.sh
+++ Build/ContinuousIntegration/create_package_zip.sh
@@ -3,11 +3,12 @@
 # This script creates the installable .zip files that are posted in the S3 bucket
 # TODO: Update this script to handle multiple worlds
 
-# Copy configuration files into the output directory
-cp holodeck-configs/DefaultWorlds/*.json dist/*
 
 # Create the zip file
 cd dist
+
+# Copy configuration files into the output directory
+cp ../holodeck-configs/DefaultWorlds/*.json .
 
 zip -r DefaultWorlds.zip *
 
