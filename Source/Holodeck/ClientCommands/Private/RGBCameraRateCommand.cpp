671be1a2fef9659dc3c8e0c9af45cee41b498aff
Jayden Milne <jaydenmilne@users.noreply.github.com>
2019-07-30 13:27:01 -0600

    Use defined identifier

--- Source/Holodeck/ClientCommands/Private/RGBCameraRateCommand.cpp
+++ Source/Holodeck/ClientCommands/Private/RGBCameraRateCommand.cpp
@@ -11,7 +11,7 @@ void URGBCameraRateCommand::Execute() {
 
 	AHolodeckGameMode* GameTarget = static_cast<AHolodeckGameMode*>(Target);
 
-	verifyf(GameTarget != nullptr, TEXT("%s UCommand::Target is not a UHolodeckGameMode*."), TEXT(__FUNCTION__));
+	verifyf(GameTarget != nullptr, TEXT("%s UCommand::Target is not a UHolodeckGameMode*."), TEXT(__func__));
 
 	UWorld* World = Target->GetWorld();
 	verify(World);
@@ -19,15 +19,15 @@ void URGBCameraRateCommand::Execute() {
 	FString AgentName = StringParams[0].c_str();
 	int ticksPerCapture = NumberParams[0];
 
-	verifyf(ticksPerCapture > 0, TEXT("%s Invalid ticks per capture provided!"), TEXT(__FUNCTION__));
+	verifyf(ticksPerCapture > 0, TEXT("%s Invalid ticks per capture provided!"), TEXT(__func__));
 
 	AHolodeckAgent* Agent = GetAgent(AgentName);
 
-	verifyf(Agent, TEXT("%s Could not find agent %s"), TEXT(__FUNCTION__), *AgentName);
+	verifyf(Agent, TEXT("%s Could not find agent %s"), TEXT(__func__), *AgentName);
 
 	FString SensorName = StringParams[1].c_str();
 
-	verifyf(Agent->SensorMap.Contains(SensorName), TEXT("%s Sensor %s not found on agent %s"), TEXT(__FUNCTION__), *SensorName, *AgentName);
+	verifyf(Agent->SensorMap.Contains(SensorName), TEXT("%s Sensor %s not found on agent %s"), TEXT(__func__), *SensorName, *AgentName);
 
 	URGBCamera* Camera = (URGBCamera*)Agent->SensorMap[SensorName];
 	Camera->TicksPerCapture = ticksPerCapture;
