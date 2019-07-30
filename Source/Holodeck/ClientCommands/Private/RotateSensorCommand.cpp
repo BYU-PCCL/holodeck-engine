fb042140fd204b095510f1caa2787427e5381d4b
Jayden Milne <jaydenmilne@users.noreply.github.com>
2019-07-30 10:32:43 -0600

    Fix RotateSensorCommand
    Was using the incorrect indexes. Added some error checking

--- Source/Holodeck/ClientCommands/Private/RotateSensorCommand.cpp
+++ Source/Holodeck/ClientCommands/Private/RotateSensorCommand.cpp
@@ -5,7 +5,7 @@
 #include "RotateSensorCommand.h"
 
 void URotateSensorCommand::Execute() {
-	UE_LOG(LogHolodeck, Log, TEXT("URotateSensorCommand::Rotate sensor"));
+	UE_LOG(LogHolodeck, Log, TEXT("URotateSensorCommand::Execute sensor"));
 
 	if (StringParams.size() != 2 || NumberParams.size() != 3) {
 		UE_LOG(LogHolodeck, Error, TEXT("Unexpected argument length found in URotateSensorCommand. Command not executed."));
@@ -16,13 +16,18 @@ void URotateSensorCommand::Execute() {
 	FString SensorName = StringParams[1].c_str();
 
 	AHolodeckAgent* Agent = GetAgent(AgentName);
+	verifyf(Agent, TEXT("%s: Could not find an agent with that name! %s"), TEXT(__FUNCTION__), *AgentName);
+
+	verifyf(Agent->SensorMap.Contains(SensorName), TEXT("%s: Could not find a sensor with that name! %s"), TEXT(__FUNCTION__), *SensorName);
+
 	UHolodeckSensor* Sensor = Agent->SensorMap[SensorName];
 
 	// Coordinates from the python side come in roll (x), pitch (y), yaw, (z) order
-	float RotationRoll = NumberParams[3];
-	float RotationPitch = NumberParams[4];
-	float RotationYaw = NumberParams[5];
+	float RotationRoll = NumberParams[0];
+	float RotationPitch = NumberParams[1];
+	float RotationYaw = NumberParams[2];
+
+	UE_LOG(LogHolodeck, Log, TEXT("roll %d pitch %d yaw %d"), RotationRoll, RotationPitch, RotationYaw);
 
 	Sensor->SetRelativeRotation(FRotator(RotationPitch, RotationYaw, RotationRoll));
-    
 }
