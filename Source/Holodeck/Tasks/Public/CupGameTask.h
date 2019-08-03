d2699f45f52bc70734c5558caab6d86a6b715578
MaxRobinsonTheGreat <maxdrob1@gmail.com>
2019-08-02 22:22:23 -0600

    Added rad documentation

--- Source/Holodeck/Tasks/Public/CupGameTask.h
+++ Source/Holodeck/Tasks/Public/CupGameTask.h
@@ -10,8 +10,9 @@
 * UCupGameTask
 * Inherits from the TaskSensor class.
 * Initializes the cup game in the world and calculates reward based off of which cup is selected
-* A reward of 1 is given if the correct cup is touched and no other cups are touched, and a reward
-* of 2 and terminal is given when the ball itself is touched and no incorrect cups are touched.
+* A reward of 1 is given for one tick if the correct cup is touched and no other cups are touched,
+* A reward of 2 and terminal is given when the ball itself is touched and no incorrect cups are touched.
+* A reward of -1 and terminal is given when an incorrect cup is touched.
 */
 UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class HOLODECK_API UCupGameTask : public UTaskSensor
