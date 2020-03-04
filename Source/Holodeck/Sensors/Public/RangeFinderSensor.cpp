84b727a60aacf92c08988a0067c3ac9bb48e627b
Kolby Nottingham <kolbytn@gmail.com>
2020-03-04 11:39:56 -0700

    Init commit

---
+++ Source/Holodeck/Sensors/Public/RangeFinderSensor.cpp
@@ -0,0 +1,5 @@
+// MIT License (c) 2019 BYU PCCL see LICENSE file
+
+#include "Holodeck.h"
+#include "RangeFinderSensor.h"
+
---
+++ Source/Holodeck/Sensors/Public/RangeFinderSensor.h
@@ -0,0 +1,17 @@
+// MIT License (c) 2019 BYU PCCL see LICENSE file
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "HolodeckCore/Public/HolodeckSensor.h"
+#include "RangeFinderSensor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOLODECK_API URangeFinderSensor : public UHolodeckSensor
+{
+	GENERATED_BODY()
+	
+};
