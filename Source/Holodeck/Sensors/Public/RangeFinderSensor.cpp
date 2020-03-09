af333e95b3ad19ec3107d7074a3797346fe206d6
Kolby Nottingham <kolbytn@gmail.com>
2020-03-09 17:30:40 -0600

    RangeFinder logic

--- Source/Holodeck/Sensors/Public/RangeFinderSensor.cpp
+++ Source/Holodeck/Sensors/Public/RangeFinderSensor.cpp
@@ -2,4 +2,64 @@
 
 #include "Holodeck.h"
 #include "RangeFinderSensor.h"
+#include "Json.h"
 
+URangeFinderSensor::URangeFinderSensor() {
+	SensorName = "RangeFinderSensor";
+}
+
+// Allows sensor parameters to be set programmatically from client.
+void URangeFinderSensor::ParseSensorParms(FString ParmsJson) {
+	Super::ParseSensorParms(ParmsJson);
+
+	TSharedPtr<FJsonObject> JsonParsed;
+	TSharedRef<TJsonReader<TCHAR>> JsonReader = TJsonReaderFactory<TCHAR>::Create(ParmsJson);
+	if (FJsonSerializer::Deserialize(JsonReader, JsonParsed)) {
+
+		if (JsonParsed->HasTypedField<EJson::Number>("LazerCount")) {
+			LazerCount = JsonParsed->GetIntegerField("LazerCount");
+		}
+
+		if (JsonParsed->HasTypedField<EJson::Number>("LazerAngle")) {
+			LazerAngle = JsonParsed->GetIntegerField("LazerAngle");
+		}
+
+		if (JsonParsed->HasTypedField<EJson::Number>("LazerMaxDistance")) {
+			LazerMaxDistance = JsonParsed->GetIntegerField("LazerMaxDistance");
+		}
+	}
+	else {
+		UE_LOG(LogHolodeck, Fatal, TEXT("URangeFinderSensor::ParseSensorParms:: Unable to parse json."));
+	}
+}
+
+void URangeFinderSensor::InitializeSensor() {
+	Super::InitializeSensor();
+	//You need to get the pointer to the object you are attached to. 
+	Parent = this->GetAttachmentRootActor();
+}
+
+void URangeFinderSensor::TickSensorComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) {
+
+	float* FloatBuffer = static_cast<float*>(Buffer);
+
+	for (int i = 0; i < LazerCount; i++) {
+
+		FVector start = GetForwardVector();
+		FVector right = GetRightVector();
+		start = start.RotateAngleAxis(360 * i / LazerCount, GetUpVector());
+		right = right.RotateAngleAxis(360 * i / LazerCount, GetUpVector());
+		start.RotateAngleAxis(LazerAngle, right);
+
+		FVector end = start * LazerMaxDistance;
+
+		FCollisionQueryParams QueryParams = FCollisionQueryParams();
+		QueryParams.AddIgnoredActor(Parent);
+
+		FHitResult Hit = FHitResult();
+
+		bool TraceResult = GetWorld()->LineTraceSingleByChannel(Hit, start, end, ECollisionChannel::ECC_Visibility, QueryParams);
+
+		FloatBuffer[i] = Hit.Distance != NULL ? Hit.Distance : LazerMaxDistance;
+	}
+}
--- Source/Holodeck/Sensors/Public/RangeFinderSensor.h
+++ Source/Holodeck/Sensors/Public/RangeFinderSensor.h
@@ -7,11 +7,55 @@
 #include "RangeFinderSensor.generated.h"
 
 /**
- * 
+ * URangeFinderSensor
+ * The RangeFinderSensor gets distances to first collision.
+ * By default the sensor retruns one number that represents the distance to
+ * the first collision in the sensor's forward direction. Increasing LazerCount
+ * will create an array of sensors evenly distributed 360 degrees in a plane 
+ * around the agent. The LazerAngle offsets each of these and transforms the
+ * plane into a cone. The default MaxDistance is 100.
  */
 UCLASS()
 class HOLODECK_API URangeFinderSensor : public UHolodeckSensor
 {
 	GENERATED_BODY()
 	
+public:
+	/*
+	* Default Constructor
+	*/
+	URangeFinderSensor();
+
+	/**
+	* InitializeSensor
+	* Sets up the class
+	*/
+	virtual void InitializeSensor() override;
+
+	/**
+	* Allows parameters to be set dynamically
+	*/
+	virtual void ParseSensorParms(FString ParmsJson) override;
+
+protected:
+	//See HolodeckSensor for the documentation of these overridden functions.
+	int GetNumItems() override { return LazerCount; };
+	int GetItemSize() override { return sizeof(float); };
+	void TickSensorComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere)
+	int LazerCount = 1;
+
+	UPROPERTY(EditAnywhere)
+	int LazerAngle = 0;
+
+	UPROPERTY(EditAnywhere)
+	int LazerMaxDistance = 100;
+
+private:
+	/*
+	 * Parent
+	 * After initialization, Parent contains a pointer to whatever the sensor is attached to.
+	 */
+	AActor* Parent;
 };
